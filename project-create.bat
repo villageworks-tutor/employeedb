a05492df99ed04b3cbc9868f8c28c77f23a808fc
villageworks-tutor <villageworks-tutor@gmail.com>
2021-09-25 16:11:25 +0900

    second commit

---
+++ project-create.bat
@@ -0,0 +1,11 @@
+type .gitkeep > .gitkeep
+mkdir public_html
+copy .gitkeep > ./public_html/.gitkeep
+mkdir application
+copy .gitkeep > ./application/.gitkeep
+mkdir test
+copy .gitkeep > ./test/.gitkeep
+mkdir compass
+mkdir lib
+copy .gitkeep > ./lib/.gitkeep
+del .gitkeep
